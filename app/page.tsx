c4b719264d57a715bfe38fe1e4c88b096ca03e31
Alex Luo <alexluo92823@gmail.com>
2023-10-04 20:46:08 -0400

    add logo and font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 
 const Index = () => {
-  return <main className=""></main>;
+  return (
+    <main className="font-display text-yellow text-9xl font-bold bg-black p-12">
+      ELECTRON VOLTS
+      <div className="text-yellow font-body text-lg font-normal">hi this is me</div>
+    </main>
+  );
 };
 
 export default Index;
